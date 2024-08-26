66ea29d98e8d3322bd27f0e3659e297cc64a72ca
riyazgithubavailable <144905521+riyazgithubavailable@users.noreply.github.com>
2024-08-26 13:10:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+
+ function solve() {
+            const select  = document.querySelector("select");
+            const selectedValue = select.value ;
+            let options = select.children ;
+            for(let i= 0 ; i < options.length; i++) {
+                if(options[i].value === selectedValue){
+                    options[i].remove();
+                    break;
+                }
+            }
+        }
